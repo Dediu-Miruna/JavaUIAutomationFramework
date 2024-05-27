9823c777a94a511e0281978d8c9a797a5b8021f7
Dediu-Miruna <dediumiruna98@gmail.com>
2024-05-27 19:49:11 +0300

    TA-3_ImplementPageObjects

--- src/main/java/online/tekwillacademy/TestRunner.java
+++ src/main/java/online/tekwillacademy/TestRunner.java
@@ -1,28 +1,40 @@
 package online.tekwillacademy;
+
+import online.tekwillacademy.managers.DataGeneratorManager;
 import online.tekwillacademy.managers.DriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
+import online.tekwillacademy.pageobjects.AccountPage;
+import online.tekwillacademy.pageobjects.HomePage;
+import online.tekwillacademy.pageobjects.LoginPage;
+import online.tekwillacademy.pageobjects.RegisterPage;
+import org.openqa.selenium.*;
 
 public class TestRunner {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = DriverManager.getInstance().getDriver();
-
-        String currentTabName = driver.getWindowHandle();
-        driver.switchTo().newWindow(WindowType.TAB);
-
-        driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-firefox-driver");
-        Thread.sleep(5000);
-        driver.close();
-
-        driver.switchTo().window(currentTabName);
-
         driver.get("https://tekwillacademy-opencart.online/");
+
+        HomePage homePage = new HomePage(driver);
+        homePage.navigateToRegisterPage();
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.clickOnContinueButton();
+        String randomEmail = DataGeneratorManager.getRandomEmail();
+        registerPage.completeTheRegisterForm("Andrei", "Secu", randomEmail , "Password1234!");
+        registerPage.enablePrivacyToggle();
+        registerPage.clickOnContinueButton();
+
+        Thread.sleep(2000);
+
+        AccountPage accountPage = new AccountPage(driver);
+        accountPage.clickOnLogOutButton();
+
+        homePage.navigateToLoginPage();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.completeTheLoginForm(randomEmail, "Password1234!");
+        loginPage.clickOnContinueButton();
+
         driver.quit();
-
-
-        System.out.println("The driver is closed");
-
-
-
+        System.out.println("The test is finished and the driver is closed");
     }
 }
